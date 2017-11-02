f8d767cef8e128fb0cb20ed92ef11d9f2efe290d
Paddy Steed <paddy.steed@engineerbetter.com>
2017-11-02 12:15:19 +0000

    remove version check, not all binaries support it
    Signed-off-by: Will Gant <w_gant@yahoo.co.uk>

--- install_binaries.sh
+++ install_binaries.sh
@@ -5,6 +5,4 @@ names=( terraform cf jq om fly bosh bbl yaml cliaas )
 for name in "${names[@]}"
 do
   chmod +x /usr/bin/$name
-  sync # docker bug requires this
-  $name --version || $name --help
 done
