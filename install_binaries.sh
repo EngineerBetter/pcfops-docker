d77a58420d55976fad2fe2b50caf741d760db38b
EngineerBetter <siemens-java@engineerbetter.com>
2017-11-07 11:40:07 +0000

    undo adding cliaas

--- install_binaries.sh
+++ install_binaries.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -eux
 
-names=( terraform cf jq om fly bosh bbl yaml cliaas )
+names=( terraform cf jq om fly bosh bbl yaml )
 for name in "${names[@]}"
 do
   chmod +x /usr/bin/$name
+  sync # docker bug requires this
+  $name --version
 done
