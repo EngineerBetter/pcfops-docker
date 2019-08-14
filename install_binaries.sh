906cae7aebc362bdde6dda9e637785bdff26f6ba
Daniel Jones <daniel.jones@engineerbetter.com>
2019-08-14 09:34:26 +0100

    work around yaml2json exit 2 on --version

--- install_binaries.sh
+++ install_binaries.sh
@@ -6,5 +6,15 @@ for name in "${names[@]}"
 do
   chmod +x /usr/bin/$name
   sync # docker bug requires this
+
+  # See https://github.com/wakeful/yaml2json/issues/4
+  if [ "$name" == "yaml2json" ]; then
+    set +u
+  fi
+
   $name --version
+
+  if [ "$name" == "yaml2json" ]; then
+    set -u
+  fi
 done
