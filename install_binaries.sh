b53864d04f29ea2e394842260002482b2c8a5741
Daniel Jones <daniel.jones@engineerbetter.com>
2019-08-14 11:52:06 +0100

    Revert "e is not u, and that is why we have different letters"
    This reverts commit fa9612ad21e52a7db467c041dd91363420286c36.
    
    Revert "work around yaml2json exit 2 on --version"
    
    This reverts commit 906cae7aebc362bdde6dda9e637785bdff26f6ba.

--- install_binaries.sh
+++ install_binaries.sh
@@ -6,15 +6,5 @@ for name in "${names[@]}"
 do
   chmod +x /usr/bin/$name
   sync # docker bug requires this
-
-  # See https://github.com/wakeful/yaml2json/issues/4
-  if [ "$name" == "yaml2json" ]; then
-    set +e
-  fi
-
   $name --version
-
-  if [ "$name" == "yaml2json" ]; then
-    set -e
-  fi
 done
