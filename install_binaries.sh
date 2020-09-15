a03b5700c4225fbac5c4b249953f572274dac8b8
Colin Simmons <colin.simmons@engineerbetter.com>
2020-09-15 11:04:42 +0100

    add kapp to image

--- install_binaries.sh
+++ install_binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-names=( terraform cf jq om fly bosh bbl yq credhub certstrap yaml2json golangci-lint bbr)
+names=( terraform cf jq om fly bosh bbl yq credhub certstrap yaml2json golangci-lint bbr kapp)
 for name in "${names[@]}"
 do
   chmod +x /usr/bin/$name
