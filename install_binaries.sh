974fb389c7b3d270219e7fb3152306d623db3413
Dominic Meddick <dom.meddick@engineerbetter.com>
2022-05-10 16:53:42 +0100

    Fix: Added missing binaries to chmod list
    Signed-off-by: Marcin Kubica <marcin.kubica@engineerbetter.com>
    Signed-off-by: William Young <will.young@engineerbetter.com>

--- install_binaries.sh
+++ install_binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-names=( terraform cf jq om fly bosh bbl yq credhub certstrap yaml2json helm golangci-lint bbr kapp ytt kf)
+names=( terraform cf jq om fly bosh bbl yq credhub certstrap yaml2json helm golangci-lint bbr kapp ytt kf kubectl kbld)
 for name in "${names[@]}"
 do
   chmod +x /usr/bin/$name
